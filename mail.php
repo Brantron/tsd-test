7ec8e8b31931978d77f2cfb590e1b7d123fd262d
bvbrandon <bvbrandonbv@gmail.com>
2015-02-07 03:42:46 -0500

    fixed email

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $phone = $_POST['phone'];
 $animals = $_POST['animals'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Message: $message";
-$recipient = "bvbrandonbv@gmail.com";
+$recipient = "brendan@topshelfdesign.net";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
